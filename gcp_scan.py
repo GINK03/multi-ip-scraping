cf7194d2ca13bdab78a87b345912353d236a13d4
g ko <gim.kobayashi@gmail.com>
2017-11-23 13:29:58 +0000

    a

---
+++ gcp_scan.py
@@ -0,0 +1,23 @@
+import json
+import os
+import re
+
+data = os.popen('gcloud compute instances list --format json').read()
+print(data)
+
+name_ip = {}
+for data in json.loads( data ):
+  if data['scheduling']['preemptible'] != True:
+    continue
+  if data['status'] != 'RUNNING':
+    continue
+  try:
+    name = data['name']
+    print(  data['networkInterfaces'][0] )
+    ip = data['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+  except KeyError as e:
+    continue
+  print(name, ip )
+  name_ip[name] = ip
+
+open('gcp_name_ip.json', 'w').write( json.dumps(name_ip,indent=2) )
