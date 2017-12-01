41b6f2282f84b5c1fa22bb9fc72a2cfc8503e0a4
nardtree <gim.kobayashi@gmail.com>
2017-12-01 17:06:07 +0000

    abc

---
+++ exmaple/kakaku-com.py
@@ -0,0 +1,82 @@
+import requests
+
+import concurrent.futures
+
+import bs4
+
+import json
+
+import os
+
+import random
+
+import sys
+
+import gzip
+
+import pickle
+proxies = []
+for name_ip in open('../aws_ip.txt'):
+  name, ip = name_ip.strip().split()
+  proxies.append( {'http':'{}:8080'.format(ip), 'https':'{}:8080'.format(ip) } )
+
+
+def url_fix(url_, urls):
+  furls = set()
+  for url in urls:
+    try:
+      if url[0] == '/':
+        top = url_.split('/')[2]
+        url = 'http://' + top + '/' + url
+        furls.add(url)
+        continue
+    except Exception:
+      continue
+    if 'javascript' in url:
+      continue
+    if 'kakaku.com' not in url:
+      continue
+    furls.add(url)
+  return furls
+
+def map1(arr):
+  index, url = arr
+  save_name = 'htmls/' + url.replace('/', '_')
+  if os.path.exists(save_name) is True:
+    return set()
+  print( 'now url', url ) 
+  try:
+    r = requests.get(url, proxies=proxies[index%len(proxies)])
+  except Exception as ex:
+    print(ex)
+    return set()
+  html = r.text
+  try:
+    open( save_name, 'w' ).write( html )  
+  except OSError:
+    return set()
+  soup = bs4.BeautifulSoup(r.text)
+  urls = set()
+  for a in soup.find_all('a', href=True):
+    _url = a['href']
+    urls.add(_url)
+  #print('a', urls)
+  return url_fix(url, urls)
+
+urls = {'http://bbs.kakaku.com/bbs/K0000565942/SortID=21194172/'}
+if '--resume' in sys.argv:
+  urls = pickle.loads(gzip.decompress(open('urls.pkl.gz', 'rb').read()))
+
+while True:
+  arrs = [(index,url) for index,url in enumerate(urls)]
+  
+  nexts = set()
+  with concurrent.futures.ProcessPoolExecutor(max_workers=len(proxies)) as exe:
+    for urls in exe.map(map1, arrs):
+      for url in urls:
+        nexts.add(url)
+  #for arr in arrs:
+  #  for url in map1(arr):
+  #    nexts.add(url) 
+  print(nexts)
+  urls = nexts
---
+++ exmaple/recover-kakaku-com.py
@@ -0,0 +1,51 @@
+import glob
+
+import os
+
+import bs4
+
+from multiprocessing import Process
+
+import sys
+
+import pickle
+
+import gzip
+files = glob.glob('htmls/*')
+size = len(files)
+
+def _map(arr):
+  index, name = arr
+  link_name = 'links/' + name.split('/').pop().replace('/', '_') 
+  if os.path.exists(link_name) is True:
+    return
+
+  print('now iter', index, '/', size)
+  soup = bs4.BeautifulSoup( open(name).read() )
+ 
+  f = open(link_name, 'w')
+  for a in soup.find_all('a', href=True):
+    url = a['href']
+    try:
+      if url[0] == '/':
+        url = 'http://jin115.com' + url
+    except Exception:
+      continue
+    
+    if 'kakaku.com' not in url:
+      continue
+
+    f.write( url + '\n' ) 
+
+if '--map1' in sys.argv:
+  for index, name in enumerate(files):
+    p = Process(target=_map, args=((index, name), )) 
+    p.start()
+
+if '--fold1' in sys.argv:
+  urls = set()
+  for index, name in enumerate(glob.glob('links/*')):
+    print('now iter', index )
+    [urls.add(url) for url in open(name).read().split('\n')]
+  open('urls.pkl.gz', 'wb').write( gzip.compress(pickle.dumps(urls)) )
+
